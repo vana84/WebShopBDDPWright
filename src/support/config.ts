b49fb4155744f0992af78fc4d3f7dc11cadd0bff
Saravanan Thanikasalam <Saravanan.T@QUALITEST.LOCAL>
2022-03-10 21:50:39 +0000

    headless

--- src/support/config.ts
+++ src/support/config.ts
@@ -1,6 +1,7 @@
 import { LaunchOptions } from 'playwright';
 const browserOptions: LaunchOptions = {
   slowMo: 0,
+  headless: false,
   args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
   firefoxUserPrefs: {
     'media.navigator.streams.fake': true,
@@ -11,7 +12,7 @@ const browserOptions: LaunchOptions = {
 export const config = {
   browser: process.env.BROWSER || 'chromium',
   browserOptions,
-  BASE_URL: 'https://playwright.dev',
+  BASE_URL: 'https://testscriptdemo.com',
   IMG_THRESHOLD: { threshold: 0.4 },
   BASE_API_URL: 'https://www.boredapi.com/api/',
 };
